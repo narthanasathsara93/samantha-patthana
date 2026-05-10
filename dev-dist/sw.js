a224d0a2fb62ae73df89919c3c05916f217377e2
Narthana Sathsara <narthanasathsara93@gmail.com>
2026-05-11 00:37:00 +0900

    wip

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.g9f9cbbtcdo"
+    "revision": "0.89rgtuucpro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
